bf60e5fe2d26e4386631d2e29834b657ba846e2e
Zahid <rkrazzkhan01731@gmail.com>
2024-09-08 23:50:47 +0800

    sidebar section design end

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ import Colors from './Colors/Colors';
 
 const Sidebar = () => {
   return (
-    <div className='mx-auto absolute top-20 pt-20 pl-6 '>
+    <div className='mx-auto absolute top-20 pt-20 text-center w-1/12 border-r h-full'>
         <div>
             <h1><CiShoppingCart /></h1>
         </div>
