74505b37fde4202ced407645a5e3f5187f7df812
Zahid <rkrazzkhan01731@gmail.com>
2024-09-08 22:54:04 +0800

    sidebar section start

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Products from './Product/Products';
 import Recommended from './Recommended/Recommended';
+import Category from './Sidebar/Category/Category';
+import Sidebar from './Sidebar/Sidebar';
 
 function App() {
   return (
     <>
+      <Sidebar />
       <Navbar />
       <Recommended />
       <Products />
