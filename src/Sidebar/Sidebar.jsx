2fc30a1c44b949da531d8b804b4ffe39e2186279
Zahid <rkrazzkhan01731@gmail.com>
2024-09-08 23:42:05 +0800

    sidebar section end

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Category from './Category/Category'
+import { CiShoppingCart } from "react-icons/ci";
+import Price from './Price/Price';
+import Colors from './Colors/Colors';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='mx-auto absolute top-20 pt-20 pl-6 '>
+        <div>
+            <h1><CiShoppingCart /></h1>
+        </div>
+        <Category />
+        <Price />
+        <Colors />
+    </div>
   )
 }
 
