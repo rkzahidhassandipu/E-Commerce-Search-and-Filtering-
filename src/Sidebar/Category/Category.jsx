12f97fc57762216feb0d32f3943db708e6850abe
Zahid <rkrazzkhan01731@gmail.com>
2024-09-10 22:12:48 +0800

    Refactor To Clean Code sidebar category section end

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Input from '../../components/Input';
 
 const Category = () => {
   return (
@@ -7,27 +8,11 @@ const Category = () => {
         <h1 className='ml-3'>Category</h1>
       </div>
       <form className='text-start pl-3' action="">
-        
-        <div className='my-2'>
-          <label className="sidebar-label-container rounded cursor-pointer">
-          <input id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 rounded dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 focus:border-none mr-3" />Sneakers
-          </label>
-        </div>
-        <div className='my-2'>
-          <label className="sidebar-label-container rounded cursor-pointer">
-          <input id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 rounded dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 focus:border-none mr-3" />Flats
-          </label>
-        </div>
-        <div className='my-2'>
-          <label className="sidebar-label-container rounded cursor-pointer">
-          <input id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 rounded dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 focus:border-none mr-3" />Sandals
-          </label>
-        </div>
-        <div className='my-2'>
-          <label className="sidebar-label-container rounded cursor-pointer">
-          <input id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 rounded dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 focus:border-none mr-3" />Heels
-          </label>
-        </div>
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+        <Input />
       </form>
       
     </div>
---
+++ src/components/Input.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div>
+      <div className='my-2'>
+          <label className="sidebar-label-container rounded cursor-pointer">
+            <input id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 rounded dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 focus:border-none mr-3" />All
+          </label>
+        </div>
+    </div>
+  )
+}
+
+export default Input
