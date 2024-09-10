2288611d30323992eb8293066bbc3bd278b00d77
Zahid <rkrazzkhan01731@gmail.com>
2024-09-10 22:01:00 +0800

    Refactor To Clean Code Products section end

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { CiStar } from "react-icons/ci";
+import { FaShoppingBag } from "react-icons/fa";
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div className='w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl mt-4 lg:mt-0'>
+        <img src="https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg" alt="" />
+        <div className='pt-3'>
+          <h2>Shoes</h2>
+          <div className='flex items-center text-amber-500'>
+            <CiStar />
+            <CiStar />
+            <CiStar />
+            <CiStar />
+            <span>4</span>
+          </div>
+          <div className='flex justify-between items-center'>
+            <div className='flex'>
+              <span className='line-through'>$520</span>
+              <span>$400</span>
+            </div>
+            <div>
+              <FaShoppingBag />
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
