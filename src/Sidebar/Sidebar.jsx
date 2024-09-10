067e9dc8551812a8efaae0d5eb9415dd81444fa1
Zahid <rkrazzkhan01731@gmail.com>
2024-09-10 22:04:10 +0800

    sidebar style end

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ import Colors from './Colors/Colors';
 
 const Sidebar = () => {
   return (
-    <div className='mx-auto absolute text-center w-1/12 border-r h-full'>
+    <div className='mx-auto absolute text-center w-1/12 border-r h-auto'>
         <div className='mt-6 text-3xl cursor-pointer'>
             <h1 className='flex justify-center'><CiShoppingCart /></h1>
         </div>
