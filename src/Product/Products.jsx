d96e33f5325c40066698ec303f4f2f7c7a6fc340
Zahid <rkrazzkhan01731@gmail.com>
2024-09-11 19:22:17 +0800

    products page Card Component Refactor

--- src/Product/Products.jsx
+++ src/Product/Products.jsx
@@ -4,14 +4,7 @@ import Cart from '../components/Cart'
 const Products = () => {
   return (
     <section className='w-4/5 mx-auto flex flex-wrap justify-around box-border'>
-      <Cart />
-      <Cart />
-      <Cart />
-      <Cart />
-      <Cart />
-      <Cart />
-      <Cart />
-      <Cart />
+      
     </section>
   )
 }
