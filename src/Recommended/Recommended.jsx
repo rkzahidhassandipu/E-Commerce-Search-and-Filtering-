e8f7f378c3c0d4f95532187caf447f12395477a1
Zahid <rkrazzkhan01731@gmail.com>
2024-09-11 21:16:53 +0800

    button components start

--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Recommended = () => {
+const Recommended = ({onClickHandler, value, title}) => {
   return (
     <div className='w-4/5 mx-auto py-5'>
       <h1 className='font-semibold text-2xl'>Recommended</h1>
       <div className='mt-3'>
-        <button className='border rounded-md text-base px-2 py-1 mx-3'>All Products</button>
-        <button className='border rounded-md text-base px-2 py-1 mx-3'>Nike</button>
-        <button className='border rounded-md text-base px-2 py-1 mx-3'>Adidas</button>
-        <button className='border rounded-md text-base px-2 py-1 mx-3'>Puma</button>
-        <button className='border rounded-md text-base px-2 py-1 mx-3'>Vans</button>
+        <button className='border rounded-md text-base px-2 py-1 mx-3'
+          onClick={onClickHandler} value={}
+        >
+
+        </button>
       </div>
     </div>
   )
