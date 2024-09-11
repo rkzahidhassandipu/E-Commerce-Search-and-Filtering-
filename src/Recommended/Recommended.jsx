dff55867cffda248b6a7eb62ebed6ab3de3d9e7a
Zahid <rkrazzkhan01731@gmail.com>
2024-09-11 21:31:51 +0800

    Project is Complete

--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import Buttons from '../components/Buttons'
 
-const Recommended = () => {
+const Recommended = ({handleClick}) => {
   return (
     <div className='w-4/5 mx-auto py-5'>
       <h1 className='font-semibold text-2xl'>Recommended</h1>
       <div className='mt-3'>
-        <button className='border rounded-md text-base px-2 py-1 mx-3'
-          onClick={onClickHandler} value={value}
-        >
-          {title}
-        </button>
+        <Buttons onClickHandler={handleClick} value="" title='All Products' />
+        <Buttons onClickHandler={handleClick} value="Puma" title='Puma' />
+        <Buttons onClickHandler={handleClick} value="Adidas" title='Adidas' />
+        <Buttons onClickHandler={handleClick} value="Nike" title='Nike' />
+        <Buttons onClickHandler={handleClick} value="Vans" title='Vans' />
       </div>
     </div>
   )
